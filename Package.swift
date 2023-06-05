75f4d45c316d70f79b4318a688c0b174890a2fae
Raj <raj.balaiyan@wetransfer.com>
2023-06-05 14:23:36 +0100

    make source available to main app

--- Package.swift
+++ Package.swift
@@ -21,7 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibrary",
-            dependencies: ["Alamofire"]),
+            dependencies: ["Alamofire"],
+            path: "Sources"),
+            
         .testTarget(
             name: "MyLibraryTests",
             dependencies: ["MyLibrary"]),

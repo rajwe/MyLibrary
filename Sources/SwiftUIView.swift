f4ce9c151313b7292863f5ce1799bbfafd9534cb
Raj <raj.balaiyan@wetransfer.com>
2023-06-05 14:18:26 +0100

    adding a view

---
+++ Sources/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Raj Balaiyan on 05/06/2023.
+//
+
+import SwiftUI
+
+public struct MySwiftUIView: View {
+    public var body: some View {
+        Text("This is from MyLibrary")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
